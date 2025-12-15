7770392ea80912ce99cae96ac7f94f9d36ec1ae3
Dilay <dilay@Dilay-iMac.local>
2025-12-15 23:59:00 +0300

    Final working version

--- today.py
+++ today.py
@@ -1,11 +1,11 @@
-'''
+"""
 This is the module docstring
-'''
+"""
 
+def my_name_is():
+    """Returns your name."""
+    return "Dilay"
 
-pass  def my_buddy_is():
-    """
-    Returns your buddy's name.
-    """
-    return "Selenn"
-
+def my_buddy_is():
+    """Returns your buddy's name."""
+    return "Selenm"
